ec7c33e2e7b497c26ded5626898041e76f7d46c7
Christian Valenti <christian.valenti@uni-ulm.de>
2019-05-16 14:18:46 +0200

    Assignment 03 exercise 1a

---
+++ src/main/java/pvs/assignment03/HashMapArray.java
@@ -0,0 +1,39 @@
+package pvs.assignment03;
+
+import java.util.HashMap;
+
+/**
+ * Assignment 3 exercise 1.
+ *
+ * @author Christian
+ */
+public class HashMapArray<V> {
+  private int capacity;
+  private HashMap<Integer, V> hashMap;
+
+  public HashMapArray(int capacity) {
+    if (capacity < 0) {
+      throw new IllegalArgumentException("HashMapArray can't have capacity less than 0.");
+    }
+    this.capacity = capacity;
+    hashMap = new HashMap<>(capacity);
+  }
+
+  public void addAtIndex(int index, V value) {
+    if (index < 0 || index >= capacity) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    hashMap.put(index, value);
+  }
+
+  public V get(int index) {
+    if (index < 0 || index >= capacity) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    return hashMap.get(index);
+  }
+
+  public int size() {
+    return capacity;
+  }
+}
